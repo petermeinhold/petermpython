cfebe9b3bb9ad0104e5301f5e76cf0902da3367f
Peter Meinhold <peterm@deepspace.ucsb.edu>
2014-10-24 10:41:59 -0700

    drop planck util import, put two relevant functions into the script instead

--- generate_ffp8_cls_cmd.py
+++ generate_ffp8_cls_cmd.py
@@ -1,16 +1,54 @@
-from planck.Planck import Planck
 import sys
 import os
 import numpy as np
 import healpy as hp
-import planck_util_prm as pu
 from glob import glob
+
+def get_lfi_dx11_mask(nside):
+    maskfile=open('/global/homes/p/peterm/masks/dx11_lfi_total_mask_2048.pkl','rb')
+    tmask=cPickle.load(maskfile)
+    maskfile.close()
+    tmask=degrade_mask(tmask,nside_out=nside)
+    return tmask
+    
+def read_and_diff_files_fast(f1,f2,nside=256,tmask=None,return_map=False):
+    #assume tmask is already degraded
+    
+    mm1=hp.read_map(f1,[0,1,2],verbose=False)
+    mm2=hp.read_map(f2,[0,1,2],verbose=False)
+
+    mmm1=[]
+    mmm2=[]
+    for m1,m2 in zip(mm1,mm2):
+        m1=hp.ud_grade(hp.ma(m1),nside_out=nside)
+        m2=hp.ud_grade(hp.ma(m2),nside_out=nside)
+        tmask=m1.mask | m2.mask | tmask
+        mmm1.append(m1)
+        mmm2.append(m2)
+    
+    diff=[]
+    for m1,m2 in zip(mmm1,mmm2):
+        d=m1-m2
+        d.mask=tmask
+        diff.append(d)
+    
+    skyfrac=1-float(tmask.sum())/len(tmask)
+        
+    cldata=hp.anafast(diff)
+    cldata_out=[]
+    for cl in cldata:
+        cldata_out.append(cl/skyfrac)
+        
+    if return_map is False:
+        return cldata_out
+    if return_map is True:
+        return cldata_out,diff
 
 if __name__ == "__main__":
     nside =256
     topdir='/global/project/projectdirs/planck/data/ffp8/mc_noise/'
     pkldir='/global/homes/p/peterm/ffp8_noise_cls_1000/'
-    tmask=pu.get_lfi_dx11_mask(256)
+    tmask=get_lfi_dx11_mask(256)
     mcdirlist=np.sort(glob(topdir+freq+'/'+freq+'_0*'))
     homedir='/global/homes/p/peterm/'
     #arguments: freq s1,q1,hr1,s2, q2, hr2
@@ -31,7 +69,7 @@ if __name__ == "__main__":
         for mcnum in mcnumlist:
             f1=mcdir+'/ffp8_noise_%s_%s_map_mc_%s%s.fits' %(freq,s1,mcnum,hr1)
             f2=mcdir+'/ffp8_noise_%s_%s_map_mc_%s%s.fits' %(freq,s2,mcnum,hr2)
-            cls.append(pu.read_and_diff_files_fast(f1,f2,nside=256,tmask=tmask))           
+            cls.append(read_and_diff_files_fast(f1,f2,nside=256,tmask=tmask))           
     pklfilename=pkldir+'/ffp8_noise_null_cls_'+freq+s1+hr1+s2+hr2+'.pkl'
     pklfile=open(pklfilename,'wb')
     cPickle.dump(cls,pklfile)
