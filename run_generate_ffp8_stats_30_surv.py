70f0a7d9f85ecb3dafccd23e7cea9dd296a2e2dc
Peter Meinhold <peterm@deepspace.ucsb.edu>
2014-10-24 15:54:50 -0700

    run files for 30 and 44 ghz survey mc stats 1000

---
+++ run_generate_ffp8_stats_30_surv.py
@@ -0,0 +1,20 @@
+import matplotlib
+matplotlib.use("agg")
+import sys
+sys.path.append('/global/project/projectdirs/planck/software/zonca/software/testenv')
+from testenv import cluster
+    
+
+s1list=['full','full','full','full','full','full','full','full']
+s2list=['s1','s2','s3','s4','s5','s6','s7','s8']
+hr1list=['null','null','null','null','null','null','null','null']
+hr2list=['null','null','null','null','null','null','null','null']
+q1list=['null','null','null','null','null','null','null','null']
+q2list=['null','null','null','null','null','null','null','null']
+freqs=['030']
+for freq in freqs:
+    for q1,q2,s1,s2,hr1,hr2 in zip(q1list,q2list,s1list,s2list,hr1list,hr2list):
+        cmd = [
+            "python generate_ffp8_cls_cmd.py %s %s %s %s %s %s %s" % (freq,q1,s1,hr1,q2,s2,hr2)
+            ]
+        cluster.run_serial("pipe_%s_%s_%s_%s_%s_%s_%s" % (freq,q1, s1,hr1,q2,s2,hr2), "\n".join(cmd), mem=20)
