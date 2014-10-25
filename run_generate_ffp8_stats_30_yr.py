23f6a59bdd7c222c2986baa237611e59304f0ac0
Peter Meinhold <peterm@deepspace.ucsb.edu>
2014-10-24 17:12:00 -0700

    parallel cluster scripts for ffp8 mcs, years

---
+++ run_generate_ffp8_stats_30_yr.py
@@ -0,0 +1,23 @@
+import matplotlib
+matplotlib.use("agg")
+import sys
+sys.path.append('/global/project/projectdirs/planck/software/zonca/software/testenv')
+from testenv import cluster
+    
+
+
+#years
+plotdir='/global/homes/p/peterm/notebooks/plotting/nulls/years/'
+s1list=['full','full','full','full','full','yr1+yr3','full','full']
+s2list=['full','yr1','yr2','yr3','yr4','yr2+yr4','yr1+yr3','yr2+yr4']
+hr1list=['_ringhalf_1','null','null','null','null','null','null','null']
+hr2list=['_ringhalf_2','null','null','null','null','null','null','null']
+q1list=['null','null','null','null','null','null','null','null']
+q2list=['null','null','null','null','null','null','null','null']
+freqs=['030']
+for freq in freqs:
+    for q1,q2,s1,s2,hr1,hr2 in zip(q1list,q2list,s1list,s2list,hr1list,hr2list):
+        cmd = [
+            "python generate_ffp8_cls_cmd.py %s %s %s %s %s %s %s" % (freq,q1,s1,hr1,q2,s2,hr2)
+            ]
+        cluster.run_serial("pipe_%s_%s_%s_%s_%s_%s_%s" % (freq,q1, s1,hr1,q2,s2,hr2), "\n".join(cmd), mem=20)
