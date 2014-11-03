a395331b8052e4983991112ee441b85bd4c0a2e6
Peter Meinhold <peterm@deepspace.ucsb.edu>
2014-11-03 14:43:51 -0800

    runscripts for 30 and 44 survey combinations

---
+++ run_generate_ffp8_stats_30_2_surv.py
@@ -0,0 +1,22 @@
+import matplotlib
+matplotlib.use("agg")
+import sys
+sys.path.append('/global/project/projectdirs/planck/software/zonca/software/testenv')
+from testenv import cluster
+    
+
+s1list=['full','full','full','full']
+s2list=['s1','s2','s5','s6']
+s3list=['s3','s4','s7','s8']
+
+hr1list=['null','null','null','null']
+hr2list=['null','null','null','null']
+q1list=['null','null','null','null']
+q2list=['null','null','null','null']
+freqs=['030']
+for freq in freqs:
+    for q1,q2,s1,s2,hr1,hr2,s3 in zip(q1list,q2list,s1list,s2list,hr1list,hr2list,s3list):
+        cmd = [
+            "python generate_ffp8_cls_2_cmd.py %s %s %s %s %s %s %s %s" % (freq,q1,s1,hr1,q2,s2,hr2,s3)
+            ]
+        cluster.run_serial("mcstats_%s_%s_%s_%s" % (freq,s1,s2,s3), "\n".join(cmd), mem=20, hours=8)
